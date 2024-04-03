bd7be49dc6c0d81808094c8907d661d96ac346f0
halfmoon-mind <5338095@gmail.com>
2024-04-03 09:49:27 +0900

    feat: deploy command fix

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  output: "export",
   reactStrictMode: true,
 
   /**
@@ -13,10 +14,10 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
 };
 
 export default config;
